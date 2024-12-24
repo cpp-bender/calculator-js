7ed9ab0c1cd884744ce3ab25ba66248e06064720
Ahmet Altioglu <ahmetaltioglu@hotmail.com>
2024-12-24 13:16:56 +0300

    add calculator functionality

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
-console.log("Hello");
+const display = document.querySelectorAll("h1")[1];
+const resDisplay = document.getElementById("oldres");
+
+let res = "";
+let old = "";
+
+function append(input) {
+  if (display.innerHTML === null || display.innerHTML === '0') {
+      res = "";
+      display.innerHTML = "";
+  }
+  res += input;
+  display.innerHTML += input;
+}
+
+function calculate() {
+  resDisplay.style.display = "block";
+  resDisplay.innerHTML = display.innerHTML;
+  display.innerHTML = eval(res);
+}
+
+function clearDisplay() {
+  res = "";
+  old = "";
+  resDisplay.innerHTML = "";
+  display.innerHTML = res;
+}
